d2ce319cddae7593487dc0144380bb9756c93fe5
lewmoore <lewis.moore2707@gmail.com>
2018-08-02 10:40:04 +0100

    Update main.tf with storage account

--- main.tf
+++ main.tf
@@ -2,7 +2,15 @@ provider "azurerm" {
 subscription_id = "${var.subscription_id}"
 }
 
-resource "azurerm_resource_group" "StorageAcountRG" {
-  name = "StorageAcountRG"
+resource "azurerm_resource_group" "SARG" {
+  name = "storageaccountrg"
   location = "${var.location}"
+}
+
+resource "azurerm_storage_account" "AZSA" {
+  name = "insertstorageaccount"
+  resource_group_name = "${azurerm_resource_group.SARG.name}"
+  location = "${var.location}"
+  account_tier = "Standard"
+  account_replication_type = "GRS"
 }
