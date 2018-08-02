c8d26af6ca845e9fac5ca1e7fc05f6df55786b08
lewmoore <lewis.moore2707@gmail.com>
2018-08-02 10:10:29 +0100

    Add resource group resource to main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 provider "azurerm" {
 subscription_id = "${var.subscription_id}"
 }
+
+resource "azurerm_resource_group" "StorageAcountRG" {
+  name = "StorageAcountRG"
+  location = "${var.location}"
+}
